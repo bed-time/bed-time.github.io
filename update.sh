9a01a8abbe136f8f037512007765205a1b0a57af
bed-time <77277001+bed-time@users.noreply.github.com>
2021-03-11 16:45:34 -0500

    yeah
    rxt5ucf xfgtdgxfdgtxdgfxtghsgbdhhghfhghfhfhfhfhfhfhfhfhgtyhtgxdc

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
-dpkg-scanpackages -m . /dev/null > Packages
+#!/bin/sh
+dpkg-scanpackages debs /dev/null > Packages
+rm Packages.gz
 rm Packages.bz2
+gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
